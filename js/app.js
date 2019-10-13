4895a057ff0445a9c05a236e9af07bfdab6de008
Vish Potnis <vishpotnis@gmail.com>
2019-10-13 03:08:19 -0400

    adding transitions and animations

--- js/app.js
+++ js/app.js
@@ -32,36 +32,61 @@ for (let i = 0; i < cardText.length; i++) {
 
 let moves = 0; // keep track of number of user moves            
 let openCards = []; // keep track of open cards
+let userInput = true;
 
-let grid = document.querySelector('.grid-container');
+let gridState = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-
+const grid = document.querySelector('.grid-container');
 grid.addEventListener('click', handleGridClick);
+grid.addEventListener('transitionend', handleTransitionEnd);
+grid.addEventListener('transitionrun', handleTransitionRun);
 
 
+
+// handle only clicks on a card
 function handleGridClick(e) {
-    if (e.target.classList.contains("card")) {
-        handleCardClick(e.target);
+    if (userInput && e.target.classList.contains("card")) {
+        // pass card container to handler function
+        handleCardClick(e.target.parentNode);
     }
 }
 
 function handleCardClick(card) {
-    if (!card.classList.contains("match") && !card.classList.contains("open")) {
-        card.classList.add("open");
+    if (!card.classList.contains("is-flipped")) {
         openCards.push(card);
-        if (openCards.length === 2) {
-            checkCardMatch();
-        }
+        card.classList.add("is-flipped");
+        
+    }
+}
+
+function handleTransitionRun(e){
+    // userInput = false;
+    console.log(e);
+}
+
+function handleTransitionEnd(e){
+    // userInput = true;
+    if (openCards.length === 2) {
+        checkCardMatch();
     }
 }
 
 function checkCardMatch() {
     if (openCards[0].querySelector('i').className === openCards[1].querySelector('i').className) {
-        openCards[0].classList.add("match");
-        openCards[1].classList.add("match");
+        openCards[0].querySelector('.card-front').classList.add("match");
+        openCards[1].querySelector('.card-front').classList.add("match");
+        openCards[0].classList.toggle("match-animation-active");
+        openCards[1].classList.toggle("match-animation-active");
+        // openCards[0].querySelector('.card-front').classList.add("match-animation-active");
+        // openCards[1].querySelector('.card-front').classList.add("match-animation-active");
+       
     } else {
-        openCards[0].classList.remove("open");
-        openCards[1].classList.remove("open");
+        openCards[0].querySelector('.card-front').classList.add("wrong");
+        openCards[1].querySelector('.card-front').classList.add("wrong");
+        openCards[0].classList.toggle("wrong-animation-active");
+        openCards[1].classList.toggle("wrong-animation-active");
+        openCards[0].classList.remove("is-flipped");
+        openCards[1].classList.remove("is-flipped");
     }
     openCards = [];
 }
